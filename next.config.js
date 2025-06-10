649579c5e2b81f0b9adfb98b933f16eb4ec982ce
MolSal <pablomolerosalvatierra@gmail.com>
2025-06-10 22:54:58 +0200

    Configure Next.js for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/wedding-website',
+  assetPrefix: '/wedding-website/',
 };
 
 module.exports = nextConfig; 
